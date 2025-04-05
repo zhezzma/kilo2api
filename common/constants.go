9da90f56c605913a226fafd1216b80e8e67a6a95
dean <1006393151@qq.com>
2025-04-05 19:25:58 +0800

    docs

--- common/constants.go
+++ common/constants.go
@@ -3,7 +3,7 @@ package common
 import "time"
 
 var StartTime = time.Now().Unix() // unit: second
-var Version = "v1.1.7"            // this hard coding will be replaced automatically when building, no need to manually change
+var Version = "v1.1.8"            // this hard coding will be replaced automatically when building, no need to manually change
 
 type ModelInfo struct {
 	Model     string
